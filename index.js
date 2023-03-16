585b558d4dd1af83aba333ff2a2a7d0db500d090
Heikki Malkavaara <heimal@student.uef.fi>
2023-03-16 12:08:32 +0200

    tehtävä 3.9 tehty

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
+app.use(express.static('build'))
 app.use(morgan(function (tokens, req, res) {
     let logged = [
         tokens.method(req, res),
@@ -107,7 +108,7 @@ app.post('/api/persons', (req, res) => {
     res.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
