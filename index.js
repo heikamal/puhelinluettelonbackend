4dd665f8cd0ace64c3158a1701cc35b48db6628c
Heikki Malkavaara <heimal@student.uef.fi>
2023-03-16 16:26:46 +0200

    tehtävät 3.15-3.16 tehty

--- index.js
+++ index.js
@@ -60,47 +60,28 @@ app.get('/info', (req, res) => {
 })
 
 // yksittäisen henkilön esittäminen
-app.get('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
-    const person = persons.find(person => person.id === id)
-
-    if (person) {
-        res.json(person)
-    } else {
-        res.status(404).end()
-    }
+app.get('/api/persons/:id', (req, res, next) => {
+    Person.findById(req.params.id).then(person => {
+        if (person) {
+            res.json(person)
+        } else {
+            res.status(404).end()
+        }
+    }).catch(error => next(error))
 })
 
 // yksittäisen henkilön poisto
-app.delete('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
-    persons = persons.filter(person => person.id !== id)
-
-    res.status(204).end()
+app.delete('/api/persons/:id', (req, res, next) => {
+    Person.findByIdAndRemove(req.params.id)
+    .then(result => {
+      res.status(204).end()
+    })
+    .catch(error => next(error))
 })
-
-const generateId = () => {
-    const max = 1000000
-    return Math.floor(Math.random() * max)
-}
 
 // henkilön lisääminen
 app.post('/api/persons', (req, res) => {
     const body = req.body
-
-    // jos nimi tai numero puuttuu
-    if (!body.name || !body.number) {
-        return res.status(400).json({ 
-          error: 'content missing' 
-        })
-    }
-
-    // jos numero löytyy jo
-    if (persons.some(person => person.name === body.name)){
-        return res.status(400).json({ 
-            error: 'name must be unique' 
-          })
-    }
 
     const person = new Person({
         name: body.name,
@@ -111,6 +92,25 @@ app.post('/api/persons', (req, res) => {
         res.json(savedPerson)
     })
 })
+
+// virheidenkäsittelijät
+const uknownEndpoint = (req, res) => {
+    res.status(404).send({ error: 'uknown endpoint' })
+}
+
+app.use(uknownEndpoint)
+
+const errorHandler = (error, req, res, next) => {
+    console.error(error.message)
+  
+    if (error.name === 'CastError') {
+      return res.status(400).send({ error: 'malformatted id' })
+    }
+  
+    next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
