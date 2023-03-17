e3b68c844ea7ff1aeed46044898f20a38a68bb32
Heikki Malkavaara <heimal@student.uef.fi>
2023-03-17 14:29:08 +0200

    tehtävä 3.22 tehty

--- index.js
+++ index.js
@@ -3,80 +3,55 @@ const express = require('express')
 const morgan = require('morgan')
 const app = express()
 const Person = require('./models/person')
-const mongoose = require ('mongoose')
 
 app.use(express.json())
 app.use(express.static('build'))
 app.use(morgan(function (tokens, req, res) {
-    let logged = [
-        tokens.method(req, res),
-        tokens.url(req, res),
-        tokens.status(req, res),
-        tokens.res(req, res, 'content-length'), '-',
-        tokens['response-time'](req, res), 'ms'
-    ].join(' ')
+  let logged = [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'), '-',
+    tokens['response-time'](req, res), 'ms'
+  ].join(' ')
 
-    if (req.method === 'POST'){
-        logged = logged.concat(' ')
-        logged = logged.concat(JSON.stringify(req.body))
-    }
-    return logged
-  }))
-
-// puhelinluettelon alustus
-let persons = [
-    {
-        id: 1,
-        name: "Arto Hellas",
-        number: "040-123456"
-    },
-    {
-        id: 2,
-        name: "Ada Lovelace",
-        number: "39-44-5323523"
-    },
-    {
-        id: 3,
-        name: "Dan Abramov",
-        number: "12-43-234345"    
-    },
-    {
-        id: 4,
-        name: "Mary Poppendieck",
-        number: "39-23-6423122"    
-    }
-]
+  if (req.method === 'POST'){
+    logged = logged.concat(' ')
+    logged = logged.concat(JSON.stringify(req.body))
+  }
+  return logged
+}))
 
 // koko puhelinluettelon näyttäminen json-muodossa
 app.get('/api/persons', (req, res) => {
-    Person.find({}).then(people => {
-        res.json(people)
-    })
+  Person.find({}).then(people => {
+    res.json(people)
+  })
 })
 
 // info-ruudun esittäminen
 app.get('/info', (req, res) => {
-    const date = new Date()
-    Person.count({}).then(count => {
-        res.send(`Phonebook has info for ${count} people <br> ${date}`)
-    })
+  const date = new Date()
+  Person.count({}).then(count => {
+    res.send(`Phonebook has info for ${count} people <br> ${date}`)
+  })
 })
 
 // yksittäisen henkilön esittäminen
 app.get('/api/persons/:id', (req, res, next) => {
-    Person.findById(req.params.id).then(person => {
-        if (person) {
-            res.json(person)
-        } else {
-            res.status(404).end()
-        }
-    }).catch(error => next(error))
+  Person.findById(req.params.id).then(person => {
+    if (person) {
+      res.json(person)
+    } else {
+      res.status(404).end()
+    }
+  }).catch(error => next(error))
 })
 
 // yksittäisen henkilön poisto
 app.delete('/api/persons/:id', (req, res, next) => {
-    Person.findByIdAndRemove(req.params.id)
-    .then(result => {
+  Person.findByIdAndRemove(req.params.id)
+    .then(() => {
       res.status(204).end()
     })
     .catch(error => next(error))
@@ -84,53 +59,53 @@ app.delete('/api/persons/:id', (req, res, next) => {
 
 // henkilön lisääminen
 app.post('/api/persons', (req, res, next) => {
-    const body = req.body
+  const body = req.body
 
-    const person = new Person({
-        name: body.name,
-        number: body.number,
-    })
+  const person = new Person({
+    name: body.name,
+    number: body.number,
+  })
 
-    person.save().then(savedPerson => {
-        res.json(savedPerson)
-    })
+  person.save().then(savedPerson => {
+    res.json(savedPerson)
+  })
     .catch(error => next(error))
 })
 
 // henkilön numeron muokkaaminen
 app.put('/api/persons/:id', (req, res, next) => {
-    const { name, number } = req.body
-    
-    Person.findByIdAndUpdate(req.params.id, 
-        { name, number }, 
-        { new: true, runValidators: true, context: 'query' })
+  const { name, number } = req.body
+
+  Person.findByIdAndUpdate(req.params.id,
+    { name, number },
+    { new: true, runValidators: true, context: 'query' })
     .then(result => {
-        res.json(result)
+      res.json(result)
     }).catch(error => next(error))
 })
 
 // virheidenkäsittelijät
 const uknownEndpoint = (req, res) => {
-    res.status(404).send({ error: 'uknown endpoint' })
+  res.status(404).send({ error: 'uknown endpoint' })
 }
 
 app.use(uknownEndpoint)
 
 const errorHandler = (error, req, res, next) => {
-    console.error(error.message)
-  
-    if (error.name === 'CastError') {
-      return res.status(400).send({ error: 'malformatted id' })
-    } else if (error.name === 'ValidationError') {
-        return res.status(400).json({ error: error.message })
-    }
-  
-    next(error)
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return res.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return res.status(400).json({ error: error.message })
+  }
+
+  next(error)
 }
 
 app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
