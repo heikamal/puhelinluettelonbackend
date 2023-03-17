53264972280e3573ec1f08b9122f23f7fd032326
Heikki Malkavaara <heimal@student.uef.fi>
2023-03-17 12:52:26 +0200

    tehtävä 3.19 tehty

--- index.js
+++ index.js
@@ -83,7 +83,7 @@ app.delete('/api/persons/:id', (req, res, next) => {
 })
 
 // henkilön lisääminen
-app.post('/api/persons', (req, res) => {
+app.post('/api/persons', (req, res, next) => {
     const body = req.body
 
     const person = new Person({
@@ -94,20 +94,18 @@ app.post('/api/persons', (req, res) => {
     person.save().then(savedPerson => {
         res.json(savedPerson)
     })
+    .catch(error => next(error))
 })
 
 // henkilön numeron muokkaaminen
 app.put('/api/persons/:id', (req, res, next) => {
-    const body = req.body
+    const { name, number } = req.body
     
-    const person = {
-        name: body.name,
-        number: body.number,
-    }
-    
-    Person.findByIdAndUpdate(req.params.id, person, { new: true })
+    Person.findByIdAndUpdate(req.params.id, 
+        { name, number }, 
+        { new: true, runValidators: true, context: 'query' })
     .then(result => {
-        res.json(person)
+        res.json(result)
     }).catch(error => next(error))
 })
 
@@ -123,6 +121,8 @@ const errorHandler = (error, req, res, next) => {
   
     if (error.name === 'CastError') {
       return res.status(400).send({ error: 'malformatted id' })
+    } else if (error.name === 'ValidationError') {
+        return res.status(400).json({ error: error.message })
     }
   
     next(error)
